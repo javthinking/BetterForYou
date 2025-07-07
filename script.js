3013b9d509ef637092796cf99ba5e4a4da3e0784
javthinking@gmail.com <nafhan1723@gmail.com>
2025-07-07 16:44:46 +0700

    Updated

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const messages = [
-        "Are you sure?",
-        "Really sure??",
-        "Are you positive?",
-        "Sayang please.. ;(",
-        "Just think about it!",
-        "If you say no, JAP will be really sad...",
-        "I will be very sad...",
-        "I will be very very very sad...",
-        "Ok fine, I will stop asking...",
-        "Just kidding, say yes please! ❤️"
-    ];
+  const messages = [
+    "Are you sure?",
+    "Really sure??",
+    "Are you positive?",
+    "Sayang please.. ;(",
+    "Just think about it!",
+    "If you say no, JAP will be really sad...",
+    "I will be very sad...",
+    "I will be very very very sad...",
+    "Ok fine, I will stop asking...",
+    "Just kidding, say yes please! ❤️"
+  ];
 
-    let messageIndex = 0;
+  let messageIndex = 0;
 
-    document.querySelector('.no-button').addEventListener("click", function () {
-        const noButton = document.querySelector('.no-button');
-        const yesButton = document.querySelector('.yes-button');
-        noButton.textContent = messages[messageIndex];
-        messageIndex = (messageIndex + 1) % messages.length;
-        const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
-        yesButton.style.fontSize = `${currentSize * 1.5}px`;
-    });
+  document.querySelector('.no-button').addEventListener("click", function () {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+  });
 
-    document.querySelector('.yes-button').addEventListener("click", function () {
-        window.location.href = "yes_page.html";;
-    });
+  document.querySelector('.yes-button').addEventListener("click", function () {
+    window.location.href = "yes_page.html";
+  });
 });
